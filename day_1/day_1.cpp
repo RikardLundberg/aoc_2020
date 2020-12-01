7b23cdbf0132bc14227476373a83c9307f7b3b2e
RikardLundberg <rikardlundberg90@gmail.com>
2020-12-01 09:14:35 +0100

    day_1 second star

--- day_1/day_1.cpp
+++ day_1/day_1.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-void compareInputs(std::vector<int> inputs, int& a, int& b);
+void compareInputs(std::vector<int> inputs, int& a, int& b, int &c);
 
 int main()
 {
@@ -14,27 +14,34 @@ int main()
 
 	std::sort(inputs.begin(), inputs.end());
 
-	int a, b;
-	compareInputs(inputs, a, b);
+	int a, b, c;
+	compareInputs(inputs, a, b, c);
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+	std::cout << c << std::endl;
 
-	std::cout << a * b << std::endl;
+	std::cout << a * b * c << std::endl;
 }
 
-void compareInputs(std::vector<int> inputs, int& a, int& b)
+void compareInputs(std::vector<int> inputs, int& a, int& b, int &c)
 {
 	for (int input : inputs)
 	{
 		for (int i = 0; i < inputs.size(); i++) {
-			auto sum = input + inputs[i];
-			if (sum == 2020) {
-				a = input;
-				b = inputs[i];
-				return;
-			}
-			else if (sum > 2020)
+			auto preSum = input + inputs[i];
+			if (preSum > 2020)
 				break;
+			for (int j = 0; j < inputs.size(); j++) {
+				auto sum = preSum + inputs[j];
+				if (sum == 2020) {
+					a = input;
+					b = inputs[j];
+					c = inputs[i];
+					return;
+				}
+				else if (sum > 2020)
+					break;
+			}
 		}
 	}
 }
