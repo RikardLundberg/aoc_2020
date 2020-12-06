1d44d6b57ba7e845b70315e08ec58ba3bfaaf3d1
RikardLundberg <rikardlundberg90@gmail.com>
2020-12-06 07:08:30 +0100

    day_6 second star

--- day_6/day_6.cpp
+++ day_6/day_6.cpp
@@ -3,30 +3,36 @@
 #include <vector>
 #include <algorithm>
 
-void TranslateAnswers(std::string val, std::vector<char> &answers);
+void TranslateAnswers(std::string val, std::vector<char>& answers, bool initPopulation);
 int main()
 {
 	int totalAnswerCount = 0;
 	std::vector<char> answers;
 	std::string input;
+	bool initPopulation = true;
 	while (std::getline(std::cin, input) && input != "eof") {
 		if (input == "")
 		{
 			totalAnswerCount += answers.size();
 			answers.clear();
+			initPopulation = true;
 			continue;
 		}
-		TranslateAnswers(input, answers);
+		TranslateAnswers(input, answers, initPopulation);
+		initPopulation = false;
 	}
 
 	std::cout << totalAnswerCount << std::endl;
 }
 
-void TranslateAnswers(std::string val, std::vector<char>& answers)
+void TranslateAnswers(std::string val, std::vector<char>& answers, bool initPopulation)
 {
-	for (char ch : val) 
+	std::vector<char> newAnswers;
+	for (char ch : val)
 	{
-		if (!std::count(answers.begin(), answers.end(), ch))
-			answers.push_back(ch);
+		if (std::count(answers.begin(), answers.end(), ch) || initPopulation)
+			newAnswers.push_back(ch);
 	}
+	answers.clear();
+	answers = newAnswers;
 }
