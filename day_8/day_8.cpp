65aa445777778ee471dc9196072ec35629c1cd89
RikardLundberg <rikardlundberg90@gmail.com>
2020-12-08 06:40:49 +0100

    day_8 second star

--- day_8/day_8.cpp
+++ day_8/day_8.cpp
@@ -2,11 +2,9 @@
 #include <string>
 #include <vector>
 
-
+int attemptExecution(std::vector<std::string> instructions, std::vector<int> executedInstructions, int executionAttempt);
 int main()
 {
-	int acc = 0;
-
 	std::vector<std::string> instructions;
 	std::vector<int> executedInstructions;
 	std::string input;
@@ -16,23 +14,44 @@ int main()
 		executedInstructions.push_back(0);
 	}
 
-	int sp = 0;
-	while (executedInstructions[sp] != 1)
+	int executionAttempt = 0, acc = -1;
+	while(acc == -1)
+		acc = attemptExecution(instructions, executedInstructions, executionAttempt++);
+
+    std::cout << acc << std::endl;
+}
+
+int attemptExecution(std::vector<std::string> instructions, std::vector<int> executedInstructions, int executionAttempt)
+{
+	int sp = 0, acc = 0, modifier = 0;
+	while (true)
 	{
+		if (sp < 0)
+			return -1;
+		else if (sp >= instructions.size())
+			return acc;
+		else if (executedInstructions[sp] == 1)
+			return -1;
 		auto currentInstruction = instructions[sp];
 		executedInstructions[sp] = 1;
 		auto cmd = currentInstruction.substr(0, currentInstruction.find(" "));
-		auto arg = std::stoi(currentInstruction.substr(currentInstruction.find(" ")+1));
+		if (modifier == executionAttempt && (cmd == "nop" || cmd == "jmp"))
+			cmd = cmd == "nop" ? "jmp" : "nop";
+		auto arg = std::stoi(currentInstruction.substr(currentInstruction.find(" ") + 1));
 		if (cmd == "nop")
+		{
 			sp++;
-		else if (cmd == "acc") 
+			modifier++;
+		}
+		else if (cmd == "acc")
 		{
 			acc += arg;
 			sp++;
 		}
 		else if (cmd == "jmp")
+		{
 			sp += arg;
+			modifier++;
+		}
 	}
-
-    std::cout << acc << std::endl;
 }
