7acd8a7bddaa863b6a9ce8cca2ee3d5d41b7a2ca
RikardLundberg <rikardlundberg90@gmail.com>
2020-12-17 00:22:22 +0100

    day_10 second star

--- day_10/day_10.cpp
+++ day_10/day_10.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 
+void chainAdapters(std::vector<int> inputs, long long& iterations, int previous);
+void findBottlenecks(std::vector<int> inputs, std::vector<int>& bnecks);
 int main()
 {
 	int n;
@@ -12,22 +14,62 @@ int main()
 
 	std::sort(inputs.begin(), inputs.end());
 
-	auto oneC = 0;
-	auto threeC = 0;
-	auto previous = 0;
-	
+	std::vector<int> bnecks;
+	findBottlenecks(inputs, bnecks);
+
+	long long output = 1;
+
+	int lowEnd = 0;
+	int startChain = 0;
+	for (int i = 1; i < bnecks.size(); i++)
+	{
+		std::vector<int> inputSeg;
+		for (int j = lowEnd; j < bnecks[i]; j++)
+			inputSeg.push_back(inputs[j]);
+
+		long long iterations = 0;
+		chainAdapters(inputSeg, iterations, startChain);
+		output *= iterations;
+		lowEnd = bnecks[i];
+		startChain = inputs[bnecks[i]];
+	}
+
+	std::vector<int> inputSeg;
+	for (int j = lowEnd; j < inputs.size(); j++)
+		inputSeg.push_back(inputs[j]);
+
+	long long iterations = 0;
+	chainAdapters(inputSeg, iterations, startChain);
+	output *= iterations;
+
+	std::cout << output << std::endl;
+}
+
+void findBottlenecks(std::vector<int> inputs, std::vector<int>& bnecks)
+{
+	for (int i = 0; i < inputs.size(); i++)
+	{
+		if ((i == 0 || inputs[i] - inputs[i - 1] == 3)
+			&& (i == inputs.size()-1 || inputs[i + 1] - inputs[i] == 3))
+			bnecks.push_back(i);
+	}
+}
+
+void chainAdapters(std::vector<int> inputs, long long& iterations, int previous)
+{
 	for (int var : inputs)
 	{
-		if (var - previous == 1)
-			oneC++;
-		else if (var - previous == 3)
-			threeC++;
-		else if (var - previous > 3)
+		if (var <= previous)
+			continue;
+		auto diff = var - previous;
+		if (diff <= 3) {
+			if (var == inputs.back()) {
+				iterations++;
+				return;
+			}
+			chainAdapters(inputs, iterations, var);
+		}
+		else if (diff > 3)
 			break;
-		previous = var;
 	}
-	threeC++;
-	auto res = oneC* threeC;
-
-	std::cout << res << std::endl;
 }
