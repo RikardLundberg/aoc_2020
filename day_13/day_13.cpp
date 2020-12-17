fb0fa407c33e201849cc67010d186f9934a15b46
RikardLundberg <rikardlundberg90@gmail.com>
2020-12-17 17:00:15 +0100

    made faster, added time elapsed

--- day_13/day_13.cpp
+++ day_13/day_13.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
 
 void getInput(std::vector<int> &buses, std::vector<int>& poses);
 int main()
 {
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     long long int ts = 0;
     std::vector<int> buses;
     std::vector<int> poses;
@@ -12,15 +14,17 @@ int main()
 
     int busIndex = 3;
     int busIndex2 = 7;
+    int busIndex3 = 1;
 
-    int jump = buses[busIndex] * buses[busIndex2];
+    int jump = buses[busIndex] * buses[busIndex2] * buses[busIndex3];
     int startValue = buses[busIndex];
     bool foundEquilibrium = false;
     while (!foundEquilibrium)
     {
         startValue++;
         if (startValue % buses[busIndex] == poses[busIndex]
-            && startValue % buses[busIndex2] == poses[busIndex2])
+            && startValue % buses[busIndex2] == poses[busIndex2]
+            && startValue % buses[busIndex3] == poses[busIndex3])
             break;
     }
 
@@ -30,7 +34,7 @@ int main()
     {
         for (int i = 0; i < buses.size(); i++)
         {
-            if (i == busIndex || i == busIndex2)
+            if (i == busIndex || i == busIndex2 || i == busIndex3)
                 continue;
             if (( ts+ poses[i]) % buses[i] != 0)
                 break;
@@ -43,7 +47,8 @@ int main()
         ts += jump;
     }
 
-    std::cout << correctTs << std::endl;
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << correctTs << std::endl << std::endl << "Time elapsed = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms"<< std::endl;
 }
 
 
