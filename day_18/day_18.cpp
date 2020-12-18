6742783e420657e546186d4e3a437f6b3f6092fd
RikardLundberg <rikardlundberg90@gmail.com>
2020-12-18 08:56:50 +0100

    rm commented code

--- day_18/day_18.cpp
+++ day_18/day_18.cpp
@@ -17,7 +17,6 @@ int main()
 			auto paraExpr = tmpStr.substr(paraStart + 1);
 			auto evalExpr = evaluateExpr(paraExpr);
 			auto head = input.substr(0, paraStart);
-			//auto tail = input.back() == paraEnd ? "" : input.substr(paraEnd + 1);
 			auto tail = input.substr(paraEnd).size() > 1 ? input.substr(paraEnd + 1) : "";
 			input = head + std::to_string(evalExpr) + tail;
 		}
@@ -71,37 +70,5 @@ long long evaluateExpr(std::string expr)
 			retVal *= vals[i];
 	}
 
-	/*
-	while (std::find(expr.begin(), expr.end(), '+') != expr.end())
-	{
-		auto pre = expr.substr(0, expr.find('+') - 1);
-		auto post = expr.substr(0, expr.find('+') + 1);
-		auto preExpPos = std::find(pre.begin(), pre.end(), ' ') == pre.end() ? 0 : pre.find_last_of(' ');
-		auto postExpPos = std::find(post.begin(), post.end(), ' ') == post.end() ? 0 : post.find_last_of(' ');
-
-		//auto preVal = std::find(pre.begin(), pre.end(), ' ') == pre.end() ? std::stoi(pre) : std::stoi(pre.substr(pre.find_last_of(' ')));
-		//auto postVal = std::find(post.begin(), post.end(), ' ') == post.end() ? std::stoi(post) : std::stoi(post.substr(post.find(' ')));
-
-
-
-		retVal = operatorIsAdd ? retVal + tmp : retVal * tmp;
-		expr = expr.substr(expr.find('+') + 1);
-		operatorIsAdd = expr.substr(0, expr.find('+')) == "+";
-		expr = expr.substr(expr.find('+') + 1);
-	}
-
-
-
-	bool operatorIsAdd = true;
-	while (std::find(expr.begin(), expr.end(), ' ') != expr.end())
-	{
-		auto tmp = std::stoi(expr.substr(0, expr.find(' ')));
-		retVal = operatorIsAdd ? retVal + tmp : retVal * tmp;
-		expr = expr.substr(expr.find(' ') + 1);
-		operatorIsAdd = expr.substr(0, expr.find(' ')) == "+";
-		expr = expr.substr(expr.find(' ') + 1);
-	}
-	retVal = operatorIsAdd ? retVal + std::stoi(expr) : retVal * std::stoi(expr);
-	*/
 	return retVal;
 }
